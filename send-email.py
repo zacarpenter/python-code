d428dc7e9c5778f31660bbfaba22ecd870f20369
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-08-20 13:01:44 -0400

    Updated send-email.py
    Added import statements and email account info.

--- send-email.py
+++ send-email.py
@@ -1,2 +1,10 @@
 # Send an email from a Gmail account
+import smtplib
+import ssl
+from email.message import EmailMessage
 
+subject = "Email from Python"
+body = "This is a test email from Python!"
+sender_email = "zcarpenter.smtp.tester@gmail.com"
+reciever_email = "carpenterzac0810@gmail.com"
+password = input("Enter the account password: ")
