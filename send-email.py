16bbcc987886cae4eebe53ada8a56765db090154
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-08-20 13:28:56 -0400

    Updated send-email.py
    Unable to test the app. Currently the "less secure" options are not available in Gmail or Yahoo.

--- send-email.py
+++ send-email.py
@@ -1,10 +1,26 @@
-# Send an email from a Gmail account
+from email import message
 import smtplib
 import ssl
 from email.message import EmailMessage
 
 subject = "Email from Python"
 body = "This is a test email from Python!"
-sender_email = "zcarpenter.smtp.tester@gmail.com"
+# Sender email not working - can't enable less secure settings in Yahoo/Gmail
+sender_email = "zcarpenter.smtp.tester@yahoo.com"
 reciever_email = "carpenterzac0810@gmail.com"
 password = input("Enter the account password: ")
+
+message = EmailMessage()
+message["From"] = sender_email
+message["To"] = reciever_email
+message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
+
+print("Sending Email")
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, reciever_email, message.as_string())
+
+print("Success!")
